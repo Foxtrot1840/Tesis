73210965640cab51020aa8e660baebb3f708479e
Inthur252 <alejandroelisei@gmail.com>
2022-09-06 03:20:41 -0300

    Ahora el Gancho funciona mejor :)

--- Tesis/Assets/Scripts/Ale/Hook.cs
+++ Tesis/Assets/Scripts/Ale/Hook.cs
@@ -5,37 +5,71 @@ using UnityEngine;
 
 public class Hook : MonoBehaviour
 {
-    [SerializeField] private GameObject _hook;
-    [SerializeField] private Camera _camera;
-    [SerializeField] private Transform _hand;
-    private LineRenderer _line;
+    [SerializeField] Camera _camera;
+    [SerializeField] Transform _grappHook;
+    [SerializeField] Transform _handPos;
+    [SerializeField] Transform _playerBody;
+    [SerializeField] float _maxDistance;
+    [SerializeField] float _hookSpeed;
+    [SerializeField] Vector3 _offset;
+    [SerializeField] LineRenderer _line;
+
+    private bool _isGrapping, _isShooting;
+    private Vector3 _hookPoint;
 
     private void Start()
     {
-        _hook.SetActive(false);
-        _line = _hook.GetComponent<LineRenderer>();
+        _isShooting = false;
+        _isGrapping = false;
     }
 
     private void Update()
     {
-        if (Input.GetKey(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E))
         {
-            if (Physics.Raycast(_hand.position, _camera.transform.forward, out RaycastHit hit))
-            {
-                _hook.SetActive(true);
-                _hook.transform.position = hit.point;
-            }
+            ShootHook();
         }
 
-        if (Input.GetKeyUp(KeyCode.E))
+        if (_isGrapping)
         {
-            _hook.SetActive(false);
+            _grappHook.position = Vector3.Lerp(_grappHook.position, _hookPoint, _hookSpeed * Time.deltaTime);
+            if(Vector3.Distance(_grappHook.position, _hookPoint) < 0.5f)
+            {
+                _playerBody.position = Vector3.Lerp(_playerBody.position, _hookPoint - _offset, _hookSpeed * Time.deltaTime);
+                _playerBody.GetComponent<Rigidbody>().useGravity = false;
+
+                if(Vector3.Distance(_playerBody.position, _hookPoint - _offset) < 0.5f)
+                {
+                    _line.enabled = false;
+                    _isGrapping = false;
+                    _grappHook.parent = _handPos;
+                    _playerBody.GetComponent<Rigidbody>().useGravity = true;
+                    _grappHook.localPosition = Vector3.zero;
+                }
+            }
         }
     }
 
     private void LateUpdate()
     {
-        _line.SetPosition(0, _hand.position);
-        _line.SetPosition(1, _hook.transform.position);
+        _line.SetPosition(0, _handPos.position);
+        _line.SetPosition(1, _hookPoint);
+    }
+
+    void ShootHook()
+    {
+        if (_isGrapping || _isShooting) return;
+
+        _isShooting = true;
+        if (Physics.Raycast(_handPos.position, _camera.transform.forward, out RaycastHit hitInfo, _maxDistance))
+        {
+            _line.enabled = true;
+            _hookPoint = hitInfo.point;
+            _isGrapping = true;
+            _grappHook.parent = null;
+            _grappHook.LookAt(_hookPoint);
+            Debug.DrawLine(_handPos.position, _hookPoint);
+        }
+        _isShooting = false;
     }
 }
