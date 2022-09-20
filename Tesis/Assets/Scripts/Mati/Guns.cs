56cb194749ff8239d8d8d46170f4fb0b5d286c66
Matias <matiasd.morales@davinci.edu.ar>
2022-09-20 05:00:37 -0300

    Guns

--- Tesis/Assets/Scripts/Mati/Guns.cs
+++ Tesis/Assets/Scripts/Mati/Guns.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class Guns : MonoBehaviour
 {
     private float counter;
-   
+    [SerializeField] private Transform spawn;
+    [SerializeField] private Camera mainCamera;
+    
+    float range = 100;
+
+    private void Start()
+    {
+        
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (!Input.GetKeyDown(KeyCode.Mouse0)) return;
+        GunsCanvasManager.instance.Shot();
+        Debug.Log("Shot!");
+        Aiming();
+    }
+
+    void Aiming()
+    {
+        RaycastHit hit;
+        
+        if (Physics.Raycast(mainCamera.transform.position, mainCamera.transform.forward, out hit, range))
         {
-            Debug.Log("Shot!");
+            Debug.Log(hit.transform.name);
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawLine(mainCamera.transform.localPosition, mainCamera.transform.forward, Color.cyan);
     }
 }
